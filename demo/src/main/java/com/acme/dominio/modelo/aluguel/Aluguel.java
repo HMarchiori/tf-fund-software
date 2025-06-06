514123bff24f422675e4f81428ebd8d1eaa4f858
HMarchiori <hmarchiori2003@gmail.com>
2025-06-05 21:29:32 -0300

    add domain service.

--- demo/src/main/java/com/acme/dominio/modelo/aluguel/Aluguel.java
+++ demo/src/main/java/com/acme/dominio/modelo/aluguel/Aluguel.java
@@ -1,5 +1,7 @@
 package com.acme.dominio.modelo.aluguel;
 
+import com.acme.dominio.modelo.cliente.Cliente;
+import com.acme.dominio.modelo.jogo.Jogo;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +13,15 @@ public class Aluguel {
     public int identificador;
     public Date dataInicial;
     public int periodo;
+    public Jogo jogo;
+    public Cliente cliente;
 
-    public Aluguel(int identificador, Date dataInicial, int periodo) {
+    public Aluguel(int identificador, Date dataInicial, int periodo, Jogo jogo, Cliente cliente) {
         this.identificador = identificador;
         this.dataInicial = dataInicial;
         this.periodo = periodo;
+        this.jogo = jogo;
+        this.cliente = cliente;
     }
 
 }
