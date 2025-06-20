bc8b1c41c0f4fa91a488654e267182ae26043d43
HMarchiori <hmarchiori2003@gmail.com>
2025-06-20 17:58:12 -0300

    Pass HEAD to CleanEndpoints (Henrique)

--- demo/src/main/java/com/acme/adaptadores/controller/aluguel/AluguelController.java
+++ demo/src/main/java/com/acme/adaptadores/controller/aluguel/AluguelController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/acmegames/aluguel")
 public class AluguelController {
 
+    // teste
     // TODO
     // Endpoint: GET /acmegames/aluguel/valorjogo/:codigo
 
